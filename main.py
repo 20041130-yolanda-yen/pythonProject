18240e1f5ddd41fa0c4c823050acaa29e7197fb3
20041130-yolanda-yen <yolandayenck@gmail.com>
2023-09-29 01:03:18 +0800

    29/9 Split skills into their job levels

--- main.py
+++ main.py
@@ -3,94 +3,114 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 import openpyxl
+import os.path
 # ---------------------------------
 
 #By Yolanda
 #Can edit base on user input but for now using fixed url
-baseURL = "https://www.jobstreet.com.sg"
-jobName = "/software-developer-jobs"
-location = "/in-Singapore?pg=3"
-
-jobTitles = []
-jobPTimes = []
-jobLevel = []
-jobURLList = []
-jobCompany = []
-jobQuali = []
-jobSkill = []
-jobLocation = []
-
-
-print("retriving data...Please hold on...")
-for i in range(3, 5, 1):
-
-    sURL = baseURL + jobName + ("/in-Singapore?pg=%d"%i)
-    response = requests.get(sURL)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    #Get job title
-    job_titles = soup.find_all("h1", class_="z1s6m00")
-    for t in job_titles:
-        t = t.text.strip()
-        jobTitles.append(t)
-
-    #Get job post time
-    job_pTimes = soup.find_all("time", class_="z1s6m00")
-    for pT in job_pTimes:
-        times = str(pT["datetime"]).split("T")[0]
-        jobPTimes.append(times)
-
-    #Get company name
-    job_Comp = soup.find_all("a", class_="_6xa4xb0")
-    for comps in job_Comp:
-        job_compLink = comps.get('data-automation')
-        if "jobCardCompanyLink" in job_compLink:
-            jobCompany.append(comps.text.strip())
-        elif "jobCardLocationLink" in job_compLink:
-            jobLocation.append(comps.text.strip())
-
-    #For each job link, get job level and job qualification
-    job_links = soup.find_all("a", class_="z1s6m00")
-    for link in job_links:
-        job_link = link.get('href')
-        if "jobId" in job_link:
-            jobURL = baseURL + job_link  # job url
-            jobURLList.append(jobURL)
-            response = requests.get(jobURL)
-            soup = BeautifulSoup(response.text, "html.parser")
-
-            time.sleep(0.5)
-            try:
-                job_Level = soup.find(string="Career Level").findNext('span').text.strip()
-                jobLevel.append(job_Level)
-
-                job_Qual = soup.find(string="Qualification").findNext('span').text.strip()
-                jobQuali.append(job_Qual)
-
-                # Get job title
-                job_skills = soup.find_all("li")
-                mySkills = ""
-                for s in job_skills:
-                    mySkills = mySkills + s.text.strip() + ","
-                jobSkill.append(mySkills)
-
-            except:
-                print("")
-
-#--------------------------------------------------------------------------------------------------------
+# baseURL = "https://www.jobstreet.com.sg"
+# jobName = "/software-developer-jobs"
+# location = "/in-Singapore?pg=3"
+#
+# jobTitles = []
+# jobPTimes = []
+# jobLevel = []
+# jobURLList = []
+# jobCompany = []
+# jobQuali = []
+# jobSkill = []
+# jobLocation = []
+#
+#
+# print("Retriving data...This might take a while, please hold on...")
+# for i in range(3, 5, 1):
+#
+#     sURL = baseURL + jobName + ("/in-Singapore?pg=%d"%i)
+#     response = requests.get(sURL)
+#     soup = BeautifulSoup(response.text, "html.parser")
+#
+#     #Get job title
+#     job_titles = soup.find_all("h1", class_="z1s6m00")
+#     for t in job_titles:
+#         t = t.text.strip()
+#         jobTitles.append(t)
+#
+#     #Get job post time
+#     job_pTimes = soup.find_all("time", class_="z1s6m00")
+#     for pT in job_pTimes:
+#         times = str(pT["datetime"]).split("T")[0]
+#         jobPTimes.append(times)
+#
+#     #Get company name
+#     job_Comp = soup.find_all("a", class_="_6xa4xb0")
+#     for comps in job_Comp:
+#         job_compLink = comps.get('data-automation')
+#         if "jobCardCompanyLink" in job_compLink:
+#             jobCompany.append(comps.text.strip())
+#         elif "jobCardLocationLink" in job_compLink:
+#             jobLocation.append(comps.text.strip())
+#
+#     #For each job link, get job level and job qualification
+#     job_links = soup.find_all("a", class_="z1s6m00")
+#     for link in job_links:
+#         job_link = link.get('href')
+#         if "jobId" in job_link:
+#             jobURL = baseURL + job_link  # job url
+#             jobURLList.append(jobURL)
+#             response = requests.get(jobURL)
+#             soup = BeautifulSoup(response.text, "html.parser")
+#
+#             time.sleep(0.5)
+#             try:
+#                 job_Level = soup.find(string="Career Level").findNext('span').text.strip()
+#                 jobLevel.append(job_Level)
+#
+#                 job_Qual = soup.find(string="Qualification").findNext('span').text.strip()
+#                 jobQuali.append(job_Qual)
+#
+#                 # Get job title
+#                 job_skills = soup.find_all("li")
+#                 mySkills = ""
+#                 for s in job_skills:
+#                     mySkills = mySkills + s.text.strip() + ","
+#                 jobSkill.append(mySkills)
+#
+#             except:
+#                 print("")
+#
+# #--------------------------------------------------------------------------------------------------------
 
 #By Andrea:
-def excelConveter(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali, jobLocation, jobSkill,jobURLList,fileName):
-    # creating excel headers
-    columns = ['Job Title', 'Post Time', 'Job Level', 'Company Name', 'Qualifications','Location', 'Skills','Job URL']
-    # Creating dataframe for pandas to convert into excel
-    df = pd.DataFrame(list(zip(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali,jobLocation, jobSkill, jobURLList)), columns=columns)
-    # Convert dataframe into excel
-    newfileName = fileName + ".xlsx"
-    df.to_excel(newfileName)
+# def excelConveter(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali, jobLocation, jobSkill,jobURLList,fileName):
+#     # creating excel headers
+#     columns = ['Job Title', 'Post Time', 'Job Level', 'Company Name', 'Qualifications','Location', 'Skills','Job URL']
+#     # Creating dataframe for pandas to convert into excel
+#     df = pd.DataFrame(list(zip(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali,jobLocation, jobSkill, jobURLList)), columns=columns)
+#     # Convert dataframe into excel
+#     newfileName = fileName + ".xlsx"
+#     df.to_excel(newfileName)
+#
+# #calling functions to convert data into dataframe then excel
+# print("Saving to excel...")
+# excelConveter(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali,jobLocation, jobSkill, jobURLList, "Jobs")
+# print("Done!")
+# #--------------------------------------------------------------------------------------------------------
 
-#calling functions to convert data into dataframe then excel
-print("Saving to excel...")
-excelConveter(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali,jobLocation, jobSkill, jobURLList, "Jobs")
-print("Done!")
-#--------------------------------------------------------------------------------------------------------
+def splitSkillsIntoJobLevel():
+    df = pd.read_excel('jobs.xlsx')
+    df1 = df[df.duplicated('Job Level', keep=False)].groupby('Job Level')['Skills'].apply(list).reset_index()
+
+    with pd.ExcelWriter(
+            "./Jobs.xlsx",
+            mode="a",
+            engine="openpyxl",
+            if_sheet_exists='replace'
+    ) as writer:
+        for index, row in df1.iterrows():
+            mys = pd.Series(row[1])
+            myf = mys.to_frame()
+            df = pd.DataFrame(myf)
+            df.to_excel(writer, sheet_name=str(row[0]))
+
+splitSkillsIntoJobLevel()
+

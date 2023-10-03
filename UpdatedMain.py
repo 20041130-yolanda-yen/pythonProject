dd6ebcdbb586125e83361a69b28d01b5737a4e59
20041130-yolanda-yen <yolandayenck@gmail.com>
2023-10-03 19:38:45 +0800

    3/10/2023 - 7.38PM Added Keyword Modules

--- UpdatedMain.py
+++ UpdatedMain.py
@@ -3,172 +3,7 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
-import random
-
-#Keywords
-SEskills_keywords = [
-    "Python",
-    "Java",
-    "C++",
-    "C#",
-    "JavaScript",
-    "HTML",
-    "CSS",
-    "SQL",
-    "Git",
-    "Docker",
-    "Kubernetes",
-    "RESTful",
-    "APIs",
-    "AWS",
-    "Azure",
-    "Google Cloud",
-    "Linux",
-    "Unix",
-    "Shell Scripting",
-    "Data Structures",
-    "Algorithms",
-    "Object-Oriented Programming",
-    "Functional Programming",
-    "Frontend Development",
-    "Backend Development",
-    "Web Development",
-    "Mobile App Development",
-    "Database Design",
-    "NoSQL",
-    "Microservices",
-    "Agile Development",
-    "Scrum",
-    "Kanban",
-    "Continuous Integration",
-    "Continuous Deployment",
-    "Test-Driven Development",
-    "DevOps",
-    "Software Architecture",
-    "Design Patterns",
-    "Code Review",
-    "Unit Testing",
-    "Debugging",
-    "Performance Optimization",
-    "User Experience (UX)",
-    "User Interface (UI) Design",
-    "Version Control",
-    "Dependency Management",
-    "Agile Methodologies",
-    "Software Development Lifecycle",
-    "Code Optimization",
-    "Scalability",
-    "Serverless Computing",
-    "CI/CD",
-    "AI",
-    "Blockchain",
-    "IoT",
-    "Big Data",
-    "Data Science",
-    "Software Testing",
-    "Automation Testing",
-    "Containerization",
-    "Scripting",
-    "Microsoft Visual Studio",
-    "Microsoft .NET framework",
-    "UML",
-    "ASIC flow layout design",
-    "design to tape-out",
-    "EDA tools",
-    "NLP",
-    "BERT",
-    "GPT",
-    "spaCy",
-    "DOTNET",
-    "SDLC",
-    "GEM",
-    "SECS",
-    "system design",
-    "Knowledge of programming",
-    "OOP",
-    ".NET",
-    "cloud technologies",
-    "servo",
-    "motion control systems",
-    "Unreal Engine",
-    "Unity",
-    "embedded software development",
-    "quality control",
-    "node.js"
-]
-
-IEskills_keywords = [
-    "Cybersecurity",
-    "Information Security",
-    "Network Security",
-    "Security Architecture",
-    "Penetration Testing",
-    "Vulnerability Assessment",
-    "Incident Response",
-    "Security Policies",
-    "Risk Assessment",
-    "Security Auditing",
-    "Firewalls",
-    "Intrusion Detection System",
-    "IDS",
-    "Intrusion Prevention System",
-    "IPS",
-    "Security Awareness",
-    "Encryption",
-    "Access Control",
-    "Security Compliance",
-    "Security Protocols",
-    "Security Analysis",
-    "Security Incident Management",
-    "Security Operations",
-    "Identity and Access Management",
-    "IAM",
-    "Information Security Manager",
-    "Certified Ethical Hacker",
-    "Information Systems Auditor",
-    "Cloud Security Professional",
-    "Security Tools",
-    "Intrusion Detection and Prevention Systems",
-    "IDPS",
-    "Antivirus Software",
-    "Firewall Management",
-    "Network Monitoring",
-    "Forensic Analysis",
-    "Malware Analysis",
-    "Data Loss Prevention",
-    "DLP",
-    "Security Patch Management",
-    "Security Awareness Training",
-    "Security Risk Management",
-    "Secure Coding Practices",
-    "Cloud Security",
-    "Mobile Device Security",
-    "Wireless Network Security",
-    "Physical Security",
-    "Business Continuity Planning",
-    "Disaster Recovery Planning",
-    "Security Consulting",
-    "Security Governance",
-    "Security Metrics and KPIs",
-    "Threat Intelligence",
-    "Security Awareness Programs",
-    "Security Best Practices",
-    "Cybersecurity Frameworks",
-    "Security Assessment",
-    "Security Compliance",
-    "Security Strategy",
-]
-
-Q_keywords = [
-    "Vocational",
-    "Bachelor",
-    "Master"
-    "Doctoral",
-    "PhD"
-    "Certification",
-    "Diploma"
-]
-
+from mySkillKeywords import SEskills_keywords, IEskills_keywords, Q_keywords
 
 baseURL = "https://www.jobstreet.com.sg"
 location = "/in-Singapore?pg=3"
@@ -267,11 +102,12 @@ def excelConveter(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali, jobLocat
 #--------------------------------------------------------------------------------------------------------
 
 #By Yolanda
-def plotGraphAll(df):
-    df.Skills.value_counts().plot(kind='barh')
-    plt.title('Popular Skills')
-    n = random.randint(0,100)
-    plt.savefig(str(n)+'.jpg', dpi = 600) #Save the plotted image
+def plotGraphAll(df, name):
+
+    df.Skills.value_counts().iloc[:10].plot(kind='barh', edgecolor='black')
+    plt.title('Popular Skills Of %s Jobs'%name)
+    plt.subplots_adjust(left=0.214)
+    plt.savefig('Popular Skills Of %s Jobs.jpg'%name) #Save the plotted image
     plt.show()
 #--------------------------------------------------------------------------------------------------------
 
@@ -311,7 +147,11 @@ def refineSkillsReq(excelName):
     df1.to_excel(excelName,index=False) #Save the update df1 into exisiting excel
     df = pd.DataFrame(mySkills) #Turn mySkills list to dataframe (this list contains ALL skills, doesnt care from which job)
     df_new = df.rename(columns={0: 'Skills'}) #name the column in this dataframe so can easily call it later
-    plotGraphAll(df_new) #plot the graph
+
+    if (excelName=='SEJobs.xlsx'):
+        plotGraphAll(df_new, 'Software Engineer') #plot the graph
+    elif (excelName=='IEJobs.xlsx'):
+        plotGraphAll(df_new, 'Information Security')
 
 # -------------------------------------FUNCTIONS END HERE-------------------------------------
 
@@ -320,15 +160,15 @@ def refineSkillsReq(excelName):
 # scrapData(jobName)
 # excelConveter(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali,jobLocation, jobSkill, jobURLList, "SEJobs")
 # print("Done!")
-#
-#
+
+
 # jobName = "/information-security-jobs"
 # scrapData(jobName)
 # excelConveter(jobTitles, jobPTimes, jobLevel, jobCompany, jobQuali,jobLocation, jobSkill, jobURLList, "IEJobs")
 # print("Done!")
 
 refineSkillsReq('SEJobs')
-refineSkillsReq('IEJobs')
+# refineSkillsReq('IEJobs')
 # ---------------------------------CALL OF FUNCTIONS ENDS HERE---------------------------------
 
 
